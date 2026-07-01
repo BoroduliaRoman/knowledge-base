7c1f4da128243e73d630d6516ad3223a10fe74f2
romeo <boroduliaroman@gmail.com>
2026-07-01 10:01:44 +0300

    Implement topic detail page

--- knowledge_logs/urls.py
+++ knowledge_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'knowledge_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Topics page
+    path('topics/', views.topics, name='topics'),
+    # Page with detail info by each topic
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
--- knowledge_logs/views.py
+++ knowledge_logs/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
     """Homepage app Knowledge Base"""
     return render(request, 'knowledge_logs/index.html')
+
+def topics(request):
+    """Show topic list"""
+    topics = Topic.objects.order_by('date_added')
+    content = {'topics': topics}
+    return render(request, 'knowledge_logs/topics.html', content)
+
+def topic(request, topic_id):
+    """Show one topic and all notice"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'knowledge_logs/topic.html', context)
