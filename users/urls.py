cf2a529c909bd99cc73b28c8f3920b0d4113ec45
romeo <boroduliaroman@gmail.com>
2026-07-03 11:08:16 +0300

    Add user registration

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Include URL auth by default
     path('', include('django.contrib.auth.urls')),
+    # Registration page
+    path('register/', views.register, name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """Register new user"""
+    if request.method != 'POST':
+        # Show empty register form
+        form = UserCreationForm()
+    else:
+        # Fill form processing
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # Logg in and redirection to home page
+            login(request, new_user)
+            return redirect('knowledge_logs:index')
+
+    # Show empty or no valid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
